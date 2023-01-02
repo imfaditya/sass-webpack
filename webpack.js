378411fd2dc2ef9f964d9920982a14f129144c07
Imam Faraz Aditya <imamaditya2211@gmail.com>
2023-01-02 20:23:55 +0700

    implementasi pengkondisian & menambahkan copy webpack plugin

--- webpack.js
+++ webpack.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -26,6 +27,14 @@ module.exports = {
     new HtmlWebpackPlugin({
       filename: 'index.html',
       template: path.resolve(__dirname, 'src/index.html'),
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/image'),
+          to: path.resolve(__dirname, 'dist/image/')
+        }
+      ]
     })
   ]
 }
